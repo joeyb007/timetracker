bca4e6238fe3e5606e21158a1d36a4ac5dfe33e4
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-02-04 00:05:32 -0500

    fix

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
 
+# loading & defining environment variables
+load_dotenv()
 
-# setting up env vars
+TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+MY_NUMBER = os.getenv("MY_PHONE_NUMBER")
 
+# Initializing twilio client
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+# initializing fastapi
 app = FastAPI()
+
+
+if __name__ == "__main__":
+    message = client.messages.create(from_=TWILIO_NUMBER,
+                                    body="Here's your text!",
+                                    to=MY_NUMBER)
