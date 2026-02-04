58039c28f18ee1a8fe60c3329809850abac3b047
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-02-04 01:12:55 -0500

    wrote actions to db

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Form, HTTPException
 import os
 from twilio.rest import Client
 from dotenv import load_dotenv
 import re
+from db import init_db, add_activity
 
 # loading & defining environment variables
 load_dotenv()
@@ -15,9 +17,6 @@ MY_NUMBER = os.getenv("MY_PHONE_NUMBER")
 # Initializing twilio client
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-# initializing fastapi
-app = FastAPI()
-
 # Helper functions
 
 def parse_message(text: str):
@@ -27,11 +26,23 @@ def parse_message(text: str):
     activity = ' '.join(message)
     return activity, mood
 
+# lifespan (on startup)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("App starting up!")
+    init_db()
+    yield
+    print("App shutting down!")
+
+# initializing fastapi
+app = FastAPI(lifespan=lifespan)
+
 # Recieving message endpoint
 @app.post('/sms')
 def receive_sms(Body: str = Form()):
     try:
         activity, mood = parse_message(Body)
+        add_activity(activity, mood)
     except:
         client.messages.create(from_=TWILIO_NUMBER,
                                 body="Please format input correctly",
