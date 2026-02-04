23a6543c8954365437930fc61c1f40a35d3f6bde
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-02-04 00:53:22 -0500

    added db tracking

--- db.py
+++ db.py
@@ -1,5 +1,24 @@
 import sqlite3
+from datetime import datetime
 
-connection = sqlite3.connect("daily_actions.db")
+def init_db():
+    connection = sqlite3.connect("daily_actions.db")
+    cursor = connection.cursor()
+    command = "CREATE TABLE IF NOT EXISTS activities(activity_id INTEGER PRIMARY KEY," \
+    "                                                activity TEXT," \
+    "                                                mood INTEGER," \
+    "                                                timestamp TEXT)"
+    cursor.execute(command)
+    connection.commit()
+    connection.close()
 
-cursor = connection.cursor()
+def add_activity(activity, mood):
+    current_time = datetime.now().isoformat()
+    connection = sqlite3.connect("daily_actions.db")
+    cursor = connection.cursor()
+    cursor.execute(
+    "INSERT INTO activities (activity, mood, timestamp) VALUES (?, ?, ?)",
+    (activity, mood, current_time)
+)
+    connection.commit()
+    connection.close()
