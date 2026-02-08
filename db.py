d03ae680b7a77aa5b161c361e96547feb4aa7c3f
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-02-07 23:38:25 -0500

    migrating to postgres

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
-import sqlite3
 from datetime import datetime
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_db():
-    connection = sqlite3.connect("daily_actions.db")
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
-    command = "CREATE TABLE IF NOT EXISTS activities(activity_id INTEGER PRIMARY KEY," \
+    command = "CREATE TABLE IF NOT EXISTS activities(activity_id SERIAL PRIMARY KEY," \
     "                                                activity TEXT," \
     "                                                mood INTEGER," \
     "                                                timestamp TEXT)"
@@ -14,10 +18,10 @@ def init_db():
 
 def add_activity(activity, mood):
     current_time = datetime.now().isoformat()
-    connection = sqlite3.connect("daily_actions.db")
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
     cursor.execute(
-    "INSERT INTO activities (activity, mood, timestamp) VALUES (?, ?, ?)",
+    "INSERT INTO activities (activity, mood, timestamp) VALUES (%s, %s, %s)",
     (activity, mood, current_time)
 )
     connection.commit()
