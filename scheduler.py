e474b2e51bac5d55623734099bb17aa117859c45
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-02-07 23:39:39 -0500

    having scheduler send message on startup

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-
+from datetime import datetime
 def send_prompt(client, from_number, to_number):
     client.messages.create(from_=from_number,
                                 body="Whatcha up to?",
@@ -7,7 +7,7 @@ def send_prompt(client, from_number, to_number):
 
 def initialize_scheduler(client, from_number, to_number):
     scheduler = BackgroundScheduler()
-    scheduler.add_job(send_prompt, 'interval', minutes=30, args=(client, from_number, to_number), id="prompt_job")
+    scheduler.add_job(send_prompt, 'interval', minutes=30, args=(client, from_number, to_number), id="prompt_job", next_run_time=datetime.now())
     scheduler.start()
     print("Scheduler started!")
     return scheduler
